9d241671817dd161e9cca0e8a4ac3b5317b4e39e
sobol <sobolev.arnold@mail.ru>
2025-07-28 20:40:11 +0700

    Добавление дата модуля для di

---
+++ app/src/main/java/com/sobolev/bank_card_app_alfa_25/di/NetworkModule.kt
@@ -0,0 +1,35 @@
+package com.sobolev.bank_card_app_alfa_25.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.sobolev.bank_card_app_alfa_25.data.network.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://lookup.binlist.net/"
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().create()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(gson: Gson): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+}
