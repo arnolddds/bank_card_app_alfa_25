d1ac5e185ef8cd9cd5cec84075cb1d83dbe279a4
sobol <sobolev.arnold@mail.ru>
2025-07-28 16:25:55 +0700

    заинжектил репозитории в юзкейсы

--- app/src/main/java/com/sobolev/bank_card_app_alfa_25/domain/usecases/GetBinInfoUseCase.kt
+++ app/src/main/java/com/sobolev/bank_card_app_alfa_25/domain/usecases/GetBinInfoUseCase.kt
@@ -2,8 +2,11 @@ package com.sobolev.bank_card_app_alfa_25.domain.usecases
 
 import com.sobolev.bank_card_app_alfa_25.domain.entitites.BinInfo
 import com.sobolev.bank_card_app_alfa_25.domain.repository.BinInfoRepository
+import javax.inject.Inject
 
-class GetBinInfoUseCase(private val repository: BinInfoRepository) {
+class GetBinInfoUseCase @Inject constructor(
+    private val repository: BinInfoRepository
+) {
     suspend operator fun invoke(bin: String): BinInfo {
         return repository.getBinInfo(bin)
     }
