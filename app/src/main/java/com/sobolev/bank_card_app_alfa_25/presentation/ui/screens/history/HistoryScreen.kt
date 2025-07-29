3c7bc98f12cd054f17e2350164e6689c50913e7e
sobol <sobolev.arnold@mail.ru>
2025-07-29 20:47:59 +0700

    убран snackbar

--- app/src/main/java/com/sobolev/bank_card_app_alfa_25/presentation/ui/screens/history/HistoryScreen.kt
+++ app/src/main/java/com/sobolev/bank_card_app_alfa_25/presentation/ui/screens/history/HistoryScreen.kt
@@ -17,8 +17,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -26,8 +24,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,7 +34,6 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.sobolev.bank_card_app_alfa_25.R
 import com.sobolev.bank_card_app_alfa_25.presentation.ui.screens.main.BinInfoCard
-import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,8 +43,6 @@ fun HistoryScreen(
     onBackClick: () -> Unit
 ) {
     val state by viewModel.state.collectAsState()
-    val snackbarHostState = remember { SnackbarHostState() }
-    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(Unit) {
         viewModel.loadHistory()
@@ -57,7 +50,6 @@ fun HistoryScreen(
 
 
     Scaffold(
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             TopAppBar(
                 title = {
